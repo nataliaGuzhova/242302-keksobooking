66d958f21b814e09ca80632066765305a2045262
Natalia <nguzhova@gmail.com>
2017-12-13 15:32:16 +0300

    код разбит на модули

---
+++ js/card.js
@@ -0,0 +1,158 @@
+'use strict';
+
+(function() {
+    var mapCardTemplate = document.querySelector('template').content.querySelector('.map__card');
+
+    window.card = {
+        'openCardAd': function(e) {
+          openPopupCardAd(e);
+        }
+    }
+
+    document.addEventListener('click', onButtonCloseClick);
+
+
+    function openPopupCardAd(evt) {
+        var elem = evt.target.parentNode;
+        var avatarNumber = 1;
+      
+        removeActiveFlag();
+        closePopup();
+      
+        if (elem.tagName === 'BUTTON') {
+          elem.classList.add('map__pin--active');
+        }
+      
+        if (elem.hasAttribute('data-userNumber')) {
+          avatarNumber = parseInt(elem.getAttribute('data-userNumber'), 10);
+        }
+      
+        if (!elem.classList.contains('map__pin--main')) {
+          createCardAd(window.data.arrayAds[avatarNumber - 1], window.map.map);
+          elem.addEventListener('keydown', onButtonCloseEsc);
+          elem.addEventListener('keydown', onButtonCloseEnter);
+        }
+      }
+    // module3-tak1
+function createCardAd(ad, canvas) {
+    var fragment = document.createDocumentFragment();
+    var beforeElement = document.querySelector('.map__filters-container');
+  
+    fragment.appendChild(renderCardAd(ad, mapCardTemplate));
+    fragment.querySelector('.popup__close').setAttribute('tabindex', '0');
+    canvas.insertBefore(fragment, beforeElement);
+  }
+  
+  function renderCardAd(ad, template) {
+    var сardTemplate = template.cloneNode(true);
+  
+    сardTemplate.querySelector('h3').textContent = ad.offer.title;
+    сardTemplate.querySelector('small').textContent = ad.offer.address;
+    сardTemplate.querySelector('.popup__price').textContent = ad.offer.price.toString() + String.fromCharCode(8381) + '/ночь';
+    сardTemplate.querySelector('h4').textContent = translateType(ad.offer.type);
+    сardTemplate.querySelector('h4 + p').textContent = ad.offer.rooms.toString() + ' комнаты для ' + ad.offer.guests.toString() + ' гостей';
+    сardTemplate.querySelector('h4 + p + p').textContent = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
+  
+    var list = сardTemplate.querySelectorAll('.feature');
+  
+    for (var j = 0; j < list.length; j++) {
+      сardTemplate.querySelector('.popup__features').removeChild(list[j]);
+    }
+  
+    for (var i = 0; i < ad.offer.features.length; i++) {
+      var liElement = document.createElement('li');
+      liElement.classList.add('feature');
+  
+      switch (ad.offer.features[i]) {
+        case 'wifi':
+          liElement.classList.add('feature--wifi');
+          break;
+        case 'dishwasher':
+          liElement.classList.add('feature--dishwasher');
+          break;
+        case 'parking':
+          liElement.classList.add('feature--parking');
+          break;
+        case 'washer':
+          liElement.classList.add('feature--washer');
+          break;
+        case 'elevator':
+          liElement.classList.add('feature--elevator');
+          break;
+        case 'conditioner':
+          liElement.classList.add('feature--conditioner');
+          break;
+      }
+      сardTemplate.querySelector('.popup__features').appendChild(liElement);
+    }
+  debugger
+    сardTemplate.querySelector('ul + p').textContent = ad.offer.description;
+    сardTemplate.querySelector('.popup__avatar').src = ad.author.avatar;
+  
+    return сardTemplate;
+  }
+  function translateType(type) {
+    var types = ['Квартира', 'Дом', 'Бунгало'];
+    var value = '';
+  
+    switch (type) {
+      case 'flat':
+        value = types[0];
+        break;
+      case 'house':
+        value = types[1];
+        break;
+      case 'bungalo':
+        value = types[2];
+        break;
+    }
+  
+    return value;
+  }
+  function closePopupCardAd(evt) {
+    hideElement(evt.target.parentNode);
+    removeActiveFlag();
+  }
+  
+  function removeActiveFlag() {
+    var preventActiveElment = document.querySelector('.map__pin--active');
+    if (preventActiveElment) {
+      preventActiveElment.classList.remove('map__pin--active');
+    }
+  }
+  
+  function closePopup() {
+    hideElement(document.querySelector('article.popup:not(.hidden)'));
+  }
+  
+  function hideElement(element) {
+    if (element) {
+      element.classList.add('hidden');
+    }
+  }
+
+  function closePopupOnKeyboard() {
+    hideElement(document.querySelector('article.popup:not(.hidden)'));
+    removeActiveFlag();
+  }
+
+  function onButtonCloseClick(evt) {
+      var target = evt.target;
+      if (target.tagName === 'BUTTON' && target.className === 'popup__close') {
+        closePopupCardAd(evt);
+      }
+    }
+    
+    function onButtonCloseEsc(evt) {
+      if (evt.keyCode === ESC_KEYCODE) {
+        closePopupOnKeyboard();
+      }
+    }
+    
+    function onButtonCloseEnter(evt) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        closePopupOnKeyboard();
+      }
+    }
+    
+}) ();
---
+++ js/form.js
@@ -0,0 +1,120 @@
+'use strict';
+
+(function() {
+    var noticeForm = document.querySelector('.notice__form');
+    
+    window.form = {
+        'noticeForm': noticeForm,
+        'sync': function() {
+                  syncInputs();
+                },
+        'validation': function() {
+                        validation();
+                      }
+    }
+
+    function syncInputs(evt) {
+        var inputValue = evt.target.value;
+        var inputId = evt.target.id;
+      
+        switch (inputId) {
+          case 'timein':
+          case 'timeout':
+            syncTimeInOut(inputId, inputValue);
+            break;
+          case 'type':
+            syncTypeHouseMinPrice(inputValue);
+            break;
+          case 'room_number':
+            syncRoomsGuests(inputValue);
+            break;
+        }
+      }
+    
+      function syncTimeInOut(inputId, selectedTime) {
+        var timeOut = 'timeout';
+        var timeIn = 'timein';
+        var timeId = timeIn;
+      
+        if (inputId === timeIn) {
+          timeId = timeOut;
+        }
+      
+        var selector = '#' + timeId;
+        var time = document.querySelector(selector);
+        time.value = selectedTime;
+      }
+      // отправки
+
+function syncTypeHouseMinPrice(selectedTypeHouse) {
+    var inputPrice = document.querySelector('#price');
+  
+    switch (selectedTypeHouse) {
+      case 'bungalo':
+        inputPrice.min = '0';
+        break;
+      case 'flat':
+        inputPrice.min = '1000';
+        break;
+      case 'house':
+        inputPrice.min = '5000';
+        break;
+      case 'palace':
+        inputPrice.min = '10000';
+        break;
+    }
+  }
+      // Количество комнат связано с количеством гостей:
+// 1 комната — «для одного гостя»
+// 2 комнаты — «для 2-х или 1-го гостя»
+// 3 комнаты — «для 2-х, 1-го или 3-х гостей»
+// 100 комнат — «не для гостей»
+function syncRoomsGuests(selectedNumbRooms) {
+    var guests = document.querySelector('#capacity');
+  
+    switch (selectedNumbRooms) {
+      case '1':
+        guests.value = '1';
+        setDisabled(guests, [0, 1, 3]);
+        break;
+      case '2':
+        guests.value = '2';
+        setDisabled(guests, [0, 3]);
+        break;
+      case '3':
+        guests.value = '3';
+        setDisabled(guests, [3]);
+        break;
+      case '100':
+        guests.value = '0';
+        setDisabled(guests, [0, 1, 2]);
+        break;
+    }
+  }
+  
+  function setDisabled(input, value) {
+    if (input.tagName === 'SELECT') {
+      for (var j = 0; j < input.options.length; j++) {
+        input.options[j].removeAttribute('hidden', '');
+      }
+  
+      for (var i = 0; i < value.length; i++) {
+        input.options[value[i]].setAttribute('hidden', '');
+      }
+    }
+  }
+  // валидация формы объявления
+  function validation() {
+    var inputs = noticeForm.elements;
+  
+    for (var i = 0; i < inputs.length; i++) {
+      var input = inputs[i];
+  
+      if (!input.checkValidity()) {
+        input.style.border = 'solid 2px rgb(255, 0, 0)';
+      } else {
+        input.style.border = 'none';
+      }
+    }
+  }
+}) ();
