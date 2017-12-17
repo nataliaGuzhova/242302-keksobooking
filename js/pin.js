12ca7d4c929fadedbddb7732ba4520d39ca35103
Natalia <nguzhova@gmail.com>
2017-12-17 15:52:31 +0300

    добавила показ координат во время движения

--- js/pin.js
+++ js/pin.js
@@ -2,82 +2,18 @@
 
 (function () {
   var mainPin = document.querySelector('.map__pin--main');
-  var MAX_LEFT_OFFSET = 300;
-  var MAX_RIGHT_OFFSET = 900;
   var MAX_TOP_OFFSET = 100;
   var MAX_BOTTOM_OFFSET = 500;
-  var mouseStartCoords;
-  var pinSize;
+  var MAX_LEFT_OFFSET = 1200;
+  var MAX_RIGHT_OFFSET = 0;
+  var pinSize = {
+    height: mainPin.offsetHeight,
+    halfWidth: Math.floor(mainPin.offsetWidth / 2)
+  };
 
   window.pin = { };
 
   mainPin.addEventListener('mouseup', onMapPinMouseup);
-
-  function onMapPinCapture(evt) {
-    evt.preventDefault();
-
-    mouseStartCoords = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
-
-    pinSize = {
-      height: mainPin.offsetHeight,
-      halfWidth: Math.floor(mainPin.offsetWidth / 2)
-    };
-
-    document.addEventListener('mousemove', onMapPinMove);
-    document.addEventListener('mouseup', onMapPinDrop);
-  }
-
-  function onMapPinMove(moveEvt) {
-    moveEvt.preventDefault();
-
-    var shift = {
-      x: mouseStartCoords.x - moveEvt.clientX,
-      y: mouseStartCoords.y - moveEvt.clientY
-    };
-
-    mouseStartCoords = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
-    };
-
-    var endY = mainPin.offsetTop - shift.y;
-
-    if (endY < MAX_TOP_OFFSET) {
-      mainPin.style.top = MAX_TOP_OFFSET + 'px';
-    } else if (endY > MAX_BOTTOM_OFFSET) {
-      mainPin.style.top = MAX_BOTTOM_OFFSET + 'px';
-    } else {
-      mainPin.style.top = endY + 'px';
-    }
-
-    var endX = mainPin.offsetLeft - shift.x;
-
-    if (endX < MAX_LEFT_OFFSET) {
-      mainPin.style.top = MAX_LEFT_OFFSET + 'px';
-    } else if (endX > MAX_RIGHT_OFFSET) {
-      mainPin.style.top = MAX_RIGHT_OFFSET + 'px';
-    } else {
-      mainPin.style.left = endX + 'px';
-    }
-
-    var pointerX = parseInt(mainPin.style.left, 10) + pinSize.halfWidth;
-    var pointerY = parseInt(mainPin.style.top, 10) + pinSize.height;
-
-    window.pin.x = pointerX;
-    window.pin.y = pointerY;
-  }
-
-  function onMapPinDrop(dropEvt) {
-    dropEvt.preventDefault();
-
-    document.removeEventListener('mousemove', onMapPinMove);
-    document.removeEventListener('mouseup', onMapPinDrop);
-
-    window.form.showPinAdress();
-  }
 
   function onMapPinMouseup() {
     window.map.map.classList.remove('map--faded');
@@ -91,11 +27,68 @@
       mapPins[i].addEventListener('keydown', onMapPinEnter);
     }
 
+    getPinCoords();
+    window.form.showPinAdress();
+
     window.form.noticeForm.addEventListener('change', window.form.sync);
     var submit = document.querySelector('.form__submit');
     submit.addEventListener('click', window.form.validation);
 
-    mainPin.addEventListener('mousedown', onMapPinCapture);
+    mainPin.addEventListener('mousedown', onMapPinStartDrag);
+  }
+
+  var shift = {};
+
+  function onMapPinStartDrag(evt) {
+    evt.preventDefault();
+
+    shift = {
+      // pageX координата относительно документа (с учетом прокрутки) 
+      x: evt.pageX - window.pin.x,
+      y: evt.pageY - window.pin.y
+    };
+
+    document.addEventListener('mousemove', onMapPinMove);
+    document.addEventListener('mouseup', onMapPinEndDrag);
+  }
+
+  function onMapPinMove(moveEvt) {
+    moveEvt.preventDefault();
+
+    var endY = moveEvt.pageY - shift.y;
+    var endX = moveEvt.pageX - shift.x;
+
+    if (endY < MAX_TOP_OFFSET) {
+      mainPin.style.top = MAX_TOP_OFFSET + 'px';
+    } else if (endY > MAX_BOTTOM_OFFSET) {
+      mainPin.style.top = MAX_BOTTOM_OFFSET + 'px';
+    } else {
+      mainPin.style.top = endY + 'px';
+    }
+
+    if (endX < MAX_RIGHT_OFFSET - pinSize.halfWidth) {
+      mainPin.style.left = MAX_RIGHT_OFFSET + 'px';
+    } else if (endX > MAX_LEFT_OFFSET - pinSize.halfWidth) {
+      mainPin.style.left = MAX_LEFT_OFFSET + 'px';
+    } else {
+      mainPin.style.left = endX + 'px';
+    }
+
+    getPinCoords();
+    window.form.showPinAdress();
+  }
+
+  function getPinCoords() {
+    // top и left даются относительно верхнего левого угла порта просмотра + скролл + размер эл-та
+    window.pin.x = mainPin.getBoundingClientRect().left + pageXOffset + pinSize.halfWidth;
+    window.pin.y = mainPin.getBoundingClientRect().top + pageYOffset + pinSize.height;
+  }
+
+  function onMapPinEndDrag(dropEvt) {
+    dropEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMapPinMove);
+    document.removeEventListener('mouseup', onMapPinEndDrag);
   }
 
   function createMapPins(arrAds) {
